633fc4d7071f171f841f9732e0ca4de08cf06203
Marta <fpadillaes@gmail.com>
2019-01-27 19:42:58 -0400

    Login redirects to calendar on sucess amd shows message on auth error

--- src/auth/login/Login.jsx
+++ src/auth/login/Login.jsx
@@ -3,7 +3,7 @@ import { Formik, Form, Field } from 'formik';
 import { Button, withStyles, createStyles, Grid } from '@material-ui/core';
 import { TextField } from 'formik-material-ui';
 import PropTypes from 'prop-types';
-import { authenticateUser, getCurrentUser } from '../../api/cognito';
+import { authenticateUser } from '../../api/cognito';
 import { SimpleEmailRegex } from 'Utils/regex';
 
 const styles = createStyles(theme => ({
@@ -16,7 +16,7 @@ const styles = createStyles(theme => ({
     borderRadius: '20px',
     padding: '3%',
     borderColor: theme.palette.primary.main,
-    width: '30%',
+    width: '35%',
   },
   textField: {
     width: '100%',
@@ -24,108 +24,134 @@ const styles = createStyles(theme => ({
   title: {
     color: theme.palette.primary.dark,
   },
+  maingrid: {
+    height: '90vh',
+  },
+  error: {
+    color: theme.palette.secondary.dark,
+  },
 }));
 
-const Login = props => {
-  return (
-    <Grid
-      container
-      direction='column'
-      justify='center'
-      alignItems='center'
-      style={{ height: '90vh' }}
-    >
-      <Formik
-        initialValues={{
-          email: '',
-          password: '',
-        }}
-        validate={values => {
-          console.log(values);
-          let errors = {};
-          if (!values.email) {
-            errors.email = props.t('required', 'Required');
-          } else if (!SimpleEmailRegex.test(values.email)) {
-            errors.email = errors.email = props.t(
-              'invalidEmail',
-              'Invalid email address'
-            );
-          }
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
 
-          if (!values.password) {
-            errors.password = props.t('required', 'Required');
-          }
+    this.state = {
+      errorMsg: '',
+    };
+  }
+  render() {
+    return (
+      <Grid
+        className={this.props.classes.maingrid}
+        container
+        direction='column'
+        justify='center'
+        alignItems='center'
+      >
+        <Formik
+          initialValues={{
+            email: '',
+            password: '',
+          }}
+          validate={values => {
+            let errors = {};
+            if (!values.email) {
+              errors.email = this.props.t('required', 'Required');
+            } else if (!SimpleEmailRegex.test(values.email)) {
+              errors.email = errors.email = this.props.t(
+                'invalidEmail',
+                'Invalid email address'
+              );
+            }
 
-          console.log('errors:', errors);
-          return errors;
-        }}
-        onSubmit={(values, { setSubmitting }) => {
-          authenticateUser(values.email, values.password, cbVals =>
-            console.log(cbVals)
-          );
-          setTimeout(() => {
+            if (!values.password) {
+              errors.password = this.props.t('required', 'Required');
+            }
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            this.setState({ errorMsg: '' });
+            setSubmitting(true);
+            authenticateUser(values.email, values.password, cbVals => {
+              setSubmitting(false);
+              if (!cbVals) {
+                this.props.history.push('');
+              } else {
+                this.setState({ errorMsg: 'Wrong username or password' });
+              }
+            });
+
+            /*setTimeout(() => {
             setSubmitting(false);
             const user = getCurrentUser(vals =>
               console.log('callback to getCurrentUser:', vals)
             );
             console.log('user:', user);
-          }, 400);
-        }}
-      >
-        {({ isSubmitting }) => (
-          <div className={props.classes.loginDiv}>
-            <h3 className={props.classes.title}>Log in into your account</h3>
-            <Form borderColor='primary'>
-              <Grid
-                container
-                direction='column'
-                justify='flex-start'
-                alignItems='center'
-              >
-                <Field
-                  required
-                  className={props.classes.textField}
-                  type='email'
-                  name='email'
-                  label={props.t('userName', 'User Name')}
-                  margin='normal'
-                  variant='outlined'
-                  component={TextField}
-                  placeholder={props.t('userName', 'User Name')}
-                />
-                <Field
-                  className={props.classes.textField}
-                  autoComplete='current-password'
-                  type='password'
-                  name='password'
-                  label={props.t('password', 'Password') + ' *'}
-                  margin='normal'
-                  variant='outlined'
-                  component={TextField}
-                  placeholder={props.t('password', 'Password') + ' *'}
-                />
-
-                <Button
-                  className={props.classes.button}
-                  variant='contained'
-                  color='secondary'
-                  type='submit'
-                  disabled={isSubmitting}
+          }, 400);*/
+          }}
+        >
+          {({ isSubmitting }) => (
+            <div className={this.props.classes.loginDiv}>
+              <h3 className={this.props.classes.title}>
+                Log in into your account
+              </h3>
+              <Form>
+                <Grid
+                  container
+                  direction='column'
+                  justify='flex-start'
+                  alignItems='center'
                 >
-                  {props.t('submit', 'Login')}
-                </Button>
-              </Grid>
-            </Form>
-          </div>
-        )}
-      </Formik>
-    </Grid>
-  );
-};
+                  <span className={this.props.classes.error}>
+                    {this.state.errorMsg}
+                  </span>
+                  <Field
+                    required
+                    className={this.props.classes.textField}
+                    type='email'
+                    name='email'
+                    label={this.props.t('userName', 'User Name')}
+                    margin='normal'
+                    variant='outlined'
+                    component={TextField}
+                    placeholder={this.props.t('userName', 'User Name')}
+                  />
+                  <Field
+                    className={this.props.classes.textField}
+                    autoComplete='current-password'
+                    type='password'
+                    name='password'
+                    label={this.props.t('password', 'Password') + ' *'}
+                    margin='normal'
+                    variant='outlined'
+                    component={TextField}
+                    placeholder={this.props.t('password', 'Password') + ' *'}
+                  />
+
+                  <Button
+                    className={this.props.classes.button}
+                    variant='contained'
+                    color='secondary'
+                    type='submit'
+                    disabled={isSubmitting}
+                  >
+                    {this.props.t('submit', 'Login')}
+                  </Button>
+                </Grid>
+              </Form>
+            </div>
+          )}
+        </Formik>
+      </Grid>
+    );
+  }
+}
 
 Login.propTypes = {
   classes: PropTypes.any,
   t: PropTypes.any,
+  history: PropTypes.any,
 };
 
 export default withStyles(styles, { withTheme: true })(Login);
