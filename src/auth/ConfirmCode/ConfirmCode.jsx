1d1980169ced800bf2b062e90320362291cab459
Howard Powell <hpowell@deloitte.ca>
2019-06-06 14:48:09 -0300

    Addition of missing files

---
+++ src/auth/ConfirmCode/ConfirmCode.jsx
@@ -0,0 +1,172 @@
+import React from 'react';
+
+import { Formik, Form, Field } from 'formik';
+
+import { TextField } from 'formik-material-ui';
+
+import PropTypes from 'prop-types';
+import {
+  IconButton,
+  AppBar,
+  Toolbar,
+  Grid,
+  withStyles,
+  createStyles,
+  Button,
+} from '@material-ui/core';
+
+import CloseIcon from '@material-ui/icons/Close';
+
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+
+import { confirmCode } from '../../api/cognito';
+
+const styles = createStyles(theme => ({
+  root: {
+    paddingTop: 25,
+  },
+  field: {
+    paddingBottom: 5,
+  },
+  textField: {
+    width: 350,
+  },
+  spacer: {
+    paddingRight: 20,
+  },
+  button: {
+    marginLeft: '40%',
+    marginTop: 30,
+    color: 'white',
+    fontSize: '14px',
+  },
+  title: {
+    color: theme.palette.primary.dark,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing.unit * 2,
+    marginLeft: theme.spacing.unit * 2,
+    width: 100,
+  },
+  appBar: {
+    position: 'relative',
+  },
+  flex: {
+    flex: 1,
+  },
+}));
+
+class ConfirmCode extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      fullWidth: true,
+      maxWidth: 'md',
+    };
+  }
+
+  render() {
+    const { t, classes } = this.props;
+
+    return (
+      <div className={classes.root}>
+        <Dialog
+          fullWidth={this.state.fullWidth}
+          maxWidth={this.state.maxWidth}
+          open={this.props.show}
+          aria-labelledby='form-dialog-title'
+        >
+          <AppBar className={classes.appBar}>
+            <Toolbar>
+              <span className={classes.flex}>Confirm Code</span>
+              <IconButton
+                color='inherit'
+                onClick={this.props.handleClose}
+                aria-label='Close'
+              >
+                <CloseIcon />
+              </IconButton>
+            </Toolbar>
+          </AppBar>
+          <DialogContent>
+            <Grid
+              container
+              direction='column'
+              justify='flex-start'
+              alignItems='center'
+            >
+              <Formik
+                initialValues={{
+                  code: null,
+                }}
+                validate={values => {
+                  let errors = {};
+
+                  if (!values.code) {
+                    errors.first_name = t('common.required');
+                  }
+
+                  return errors;
+                }}
+                onSubmit={(values, { setSubmitting }) => {
+                  confirmCode(this.props.userName, values).then(() => {
+                    setSubmitting(false);
+
+                    this.props.handleClose();
+                  });
+                }}
+              >
+                {({ isSubmitting }) => (
+                  <Form>
+                    <Grid
+                      container
+                      direction='row'
+                      justify='flex-start'
+                      alignItems='center'
+                    >
+                      <Grid className={classes.spacer} item>
+                        <Grid className={classes.field} item>
+                          <Field
+                            component={TextField}
+                            type='text'
+                            name='code'
+                            label={t('authorize.code')}
+                            margin='normal'
+                            variant='outlined'
+                            placeholder={t('authorize.code')}
+                            className={classes.textField}
+                          />
+                        </Grid>
+                      </Grid>
+                    </Grid>
+                    <Button
+                      className={this.props.classes.button}
+                      variant='contained'
+                      color='secondary'
+                      type='submit'
+                      disabled={isSubmitting}
+                    >
+                      Confirm
+                    </Button>
+                  </Form>
+                )}
+              </Formik>
+            </Grid>
+          </DialogContent>
+        </Dialog>
+      </div>
+    );
+  }
+}
+
+ConfirmCode.propTypes = {
+  t: PropTypes.func.isRequired,
+  classes: PropTypes.object,
+  show: PropTypes.bool,
+  handleClose: PropTypes.func,
+  userName: PropTypes.string,
+};
+
+export default withStyles(styles, { withTheme: true })(ConfirmCode);
